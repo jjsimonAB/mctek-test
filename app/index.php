b6ec0a47b58ffadbceff3b294dc1333365aafe26
Jeffry J. Simon <jeffrysimon@smarttec.com.do>
2020-10-22 09:08:36 -0400

    Initial commit

---
+++ app/index.php
@@ -0,0 +1,151 @@
+<?php
+
+try {
+
+    $redis = new Redis(); 
+    $redis->connect('devcache1', 6379); 
+    $allKeys = $redis->keys("*");
+    if(empty($allKeys)){
+        echo "no data found, proceeding to fetch data...";
+        $redis->hmset("1", [
+            'name' => 'movie 1', 
+            'childs' => '2,5,6,4']);
+        $redis->hmset("2", [
+            'name' => 'movie 2'
+            ]);
+        $redis->hmset("3", [
+            'name' => 'movie 3', 
+            'childs' => '7']);
+        $redis->hmset("4", [
+            'name' => 'movie 4', 
+            'childs' => '8,15,14']);
+        $redis->hmset("5", [
+            'name' => 'series 1', 
+            'childs' => '9']);
+        $redis->hmset("6", [
+            'name' => 'series 2', 
+            'childs' => '10,3']);
+        $redis->hmset("7", [
+            'name' => 'series 3', 
+            'childs' => '13']);
+        $redis->hmset("8", [
+            'name' => 'series 4', 
+            'childs' => '']);
+        $redis->hmset("9", [
+            'name' => 'movie A', 
+            'childs' => '']);
+        $redis->hmset("10", [
+            'name' => 'movie B', 
+            'childs' => '11,12']);
+        $redis->hmset("11", [
+            'name' => 'series A', 
+            'childs' => '']);
+        $redis->hmset("12", [
+            'name' => 'series B', 
+            'childs' => '']);
+        $redis->hmset("13", [
+            'name' => 'mini series 1', 
+            'childs' => '']);
+        $redis->hmset("14", [
+            'name' => 'mini series 2', 
+            'childs' => '']);
+        $redis->hmset("15", [
+            'name' => 'series 5', 
+            'childs' => '16']);
+        $redis->hmset("16", [
+            'name' => 'movie 5', 
+            'childs' => '']);
+
+    } 
+
+    sort($allKeys);
+
+    //options
+    switch($argv[1]){
+        case 'list':
+            $keys = $allKeys[0];
+            lists($keys, $redis);
+        break;
+        case 'add':
+            echo "add flow";
+            $item = [$argv[2], (isset($argv[3]) ? $argv[3] : '')];
+            addItem($item, $redis, $allKeys);
+        break;
+        case 'delete':
+            echo "delete flow";
+            deleteItem($argv[2], $redis);
+        break;
+        default:
+            echo "invalid argument";
+    }
+
+}catch (Exception $e) {
+    echo $e;
+}
+
+function lists($keys, $redis) {
+    $hasChild = false;
+    $data = $redis->hgetall($keys);
+    $dataChilds = explode(",", $data['childs']);
+    print_r($data['name']."\n");
+    if(!empty($dataChilds)){
+        generateList($dataChilds, $redis, null);
+    }
+}
+
+function generateList($dataChilds, $redis, $counter = 0 ) {
+
+    $iterationCounter = $counter;
+    $tabs = "\t";
+    if(isset($counter)) {
+        switch($iterationCounter) {
+            case 1:
+                $tabs = "\t \t";
+            break;
+            case 2: 
+                $tabs = "\t \t";
+            break;
+            case 3: 
+                $tabs = "\t \t \t";
+            break;
+            case 4: 
+                $tabs = "\t \t \t";
+            break;
+            case 5: 
+                $tabs = "\t \t \t \t";
+            break;
+            default:   
+                null;
+        }
+    }
+
+    foreach($dataChilds as $child) {
+        $childName = $redis->hgetall($child);
+        $childSubNodes = explode(",", $childName['childs']);
+        echo $tabs.$childName['name']."\n" ;
+        if(!empty($childSubNodes[0])){
+            $iterationCounter = $iterationCounter + 1;
+            generateList($childSubNodes, $redis, $iterationCounter);
+        }
+    };
+}
+
+function addItem($item, $redis, $keys) {
+    $id = sizeof($keys) + 1;
+    $rootChilds = $redis->hgetall(1);
+    $newChilds = $rootChilds['childs'] . ",$id";
+    $redis->hset(1,'childs',$rootChilds['childs'] . ",$id");
+    $redis->hmset($id, [
+        'name' => $item[0], 
+        'childs' => (!empty($item[1]) ? $item[1] : '')]);
+}
+
+function deleteItem($id, $redis) {
+    try {
+        $redis->del($id);
+    }catch(Exception $e){
+        echo $e;
+    }
+}
+
+?>
